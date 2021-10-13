e6d7b76bc01fcb1fe941317a8a82c6c4de0e695b
isaiahroberts51 <92062860+isaiahroberts51@users.noreply.github.com>
2021-10-13 16:54:03 -0700

    Lab 4 Complete

---
+++ lab4/js/lab.js
@@ -0,0 +1,32 @@
+// pseudo code for lab 4.
+// pseudo code for an every day task and a computer task.
+// @link   URL
+// @file   This file contains pseudo code.
+// @author Evan Robel and Isaiah Roberts
+// @since  0.0.0
+
+
+
+
+//Checking your car’s oil level:
+//Find a safe place like a gas station or your driveway
+//Turn off your car
+//Pop the hood
+//Remove the dipstick
+//Check the oil level on the dipstick
+//Determine whether to add oil or not based on the level
+  //Add oil if low
+  //Do not add oil if full
+//Put the dipstick back in place whether you added oil or not
+
+
+
+
+//Creating a Connect 4 game:
+//Show the current board
+//Player inputs their move
+//Make sure their move is valid (Column isn’t full)
+//See if player wins
+//Computer inputs their move
+//Make sure their move is valid
+//See if computer wins
