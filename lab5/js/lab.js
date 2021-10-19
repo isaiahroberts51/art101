7b8d05697d33dacd5b428fc280bb5577e7369090
isaiahroberts51 <92062860+isaiahroberts51@users.noreply.github.com>
2021-10-18 17:06:47 -0700

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+// code for lab 5.
+// code that defines and calculates variables about a car .
+// @link   https://evanrobel.github.io/art101/lab5/index.html
+// @file   This file contains Java script.
+// @author Evan Robel and Isaiah Roberts
+// @since  0.0.0
+
+
+//Define variables
+make = "subaru";
+model = "legacy";
+color = "white";
+year = 2019;
+
+//Calculate the age of the car
+age = 2021 - year;
+
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
